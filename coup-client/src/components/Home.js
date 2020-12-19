b8c311eddd0c3883fe83cc467a8fc2cebdcfa5ed
Jason Sogan <jason_sogan@yahoo.com>
2020-12-19 18:26:12 -0500

    Add credits, Update game description

--- coup-client/src/components/Home.js
+++ coup-client/src/components/Home.js
@@ -10,6 +10,14 @@ export default class Home extends Component {
             <div className="homeContainer">
                 <h1>Welcome to Coup</h1>
                 <p>A game of deduction and deception</p>
+                <p>This game plays with a balanced ruleset that differs from the base game</p>
+                <ul style={{textAlign: 'left'}}>
+                    <li>Influence cards revealed and discarded are not shuffled back into the play deck and go into a discard pile.</li>
+                    <li>Contessa can block a Duke's blocking ability</li>
+                    <li>When an oponnent assassinates you, if you are holding and reveal an assassin, the opponent will also be assassinated and must discard an infleunce</li>
+                    <li>When revealing an Ambassador aftering being couped, you gain 4 coins.</li>
+                </ul>
+
                 <img src={chicken} alt="chicken-leg"/>
                 <div className="input-group-btn">
                     <Link className="home" to="/create" >Create Game</Link>
@@ -26,8 +34,18 @@ export default class Home extends Component {
 
                 
             </div>
-            <p className="footer">Made by <a className="website-link" href="https://github.com/cheneth" target="_blank" rel="noopener noreferrer">Ethan Chen</a></p>
             <p className="version-number">Beta v0.82</p>
+            <div className="footer">
+                <p>Originally created by <a className="website-link" href="https://github.com/cheneth" target="_blank" rel="noopener noreferrer">Ethan Chen</a></p>
+                
+                {`\n`}
+
+                <p>Modified by
+                    <a className="website-link" href="https://github.com/MorganMartin12" target="_blank" rel="noopener noreferrer">Morgan Martin</a>
+                    <a className="website-link" href="https://github.com/soggysogan" target="_blank" rel="noopener noreferrer">Jason Sogan</a>
+                    and <a className="website-link" href="https://github.com/RobWheaton13" target="_blank" rel="noopener noreferrer">Robert Wheaton</a>
+                </p>
+            </div>
             </>
         )
     }
