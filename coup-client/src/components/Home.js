33e28a368610f9730cac9b90edce2a0ebd1ecf19
Morgan Martin <mjma254@g.uky.edu>
2020-12-21 22:10:29 -0500

    Added logo and messed with some css

--- coup-client/src/components/Home.js
+++ coup-client/src/components/Home.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import chicken from "../assets/Chicken.svg"
 import RulesModal from './RulesModal';
-
+import logo from '../assets/Coup-removebg-preview.png'
+import '../index.css'
 export default class Home extends Component {
     render() {
         return (
             <>
             <div className="homeContainer">
-                <h1>Welcome to Coup</h1>
+                 <img style={{ width:"20%",
+  height: "auto"}}src={logo} alt="chicken-leg"/>
                 <p>A game of deduction and deception</p>
-                <p>This game plays with a balanced ruleset that differs from the base game</p>
-                <ul style={{textAlign: 'left'}}>
-                    <li>Influence cards revealed and discarded are not shuffled back into the play deck and go into a discard pile.</li>
-                    <li>Contessa can block a Duke's blocking ability</li>
-                    <li>When couped by an opponent, if you are holding and reveal an assassin, the opponent will also be assassinated and must discard an infleunce. Your assassin will be discarded.</li>
-                    <li>When revealing an Ambassador after being couped, you gain 4 coins.</li>
-                </ul>
-
-                <img src={chicken} alt="chicken-leg"/>
                 <div className="input-group-btn">
                     <Link className="home" to="/create" >Create Game</Link>
                 </div>
